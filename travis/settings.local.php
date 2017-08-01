b1c5eb9db40332349854b2e9bca94e8fa9df866c
Geoff St. Pierre <serundeputy@gmail.com>
2017-08-01 10:29:46 -0400

    initial commit

---
+++ travis/settings.local.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'databasename',
+  'username' => 'sqlusername',
+  'password' => 'sqlpassword',
+  'host' => 'localhost',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+);
