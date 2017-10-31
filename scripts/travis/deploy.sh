a7b289448450ab4d4c9d1590a2a27d3181cc6503
Mike Pirog <mike@thinktandem.io>
2017-10-30 20:19:58 -0700

    First pass on merge to master deploy script

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -2,21 +2,17 @@
 
 set -eo pipefail
 
-BRANCH=$(echo $TRAVIS_PULL_REQUEST_BRANCH | grep -v '^\(master\|[0-9]\+.x\)$')
-PR_ENV=${BRANCH:+pr-$BRANCH}
-DEFAULT_ENV=$(echo ${PR_ENV:-$TRAVIS_PULL_REQUEST_BRANCH} | tr '[:upper:]' '[:lower:]' | sed 's/[^0-9a-z-]//g' | cut -c -11 | sed 's/-$//')
+# Get the env
+PANTHEON_ENV="$1"
 
 # Merge the multidev for the PR into the dev environment
-#lando terminus -n build:env:merge "$TERMINUS_SITE.$TERMINUS_ENV" --yes
-
-# Run updatedb on the dev environment
-# lando terminus -n drush $TERMINUS_SITE.dev -- updatedb --yes
+lando terminus -n multidev:merge-to-dev $PANTHEON_SITE_NAME.$PANTHEON_ENV --updatedb
 
 # If there are any exported configuration files, then import them
 if [ -f "config/system.site.yml" ] ; then
-  # lando terminus -n drush "$TERMINUS_SITE.dev" -- config-import --yes
+  lando terminus -n drush "$PANTHEON_SITE_NAME.dev" -- config-import --yes
 fi
 
 # Delete old multidev environments associated with a PR that has been
 # merged or closed.
-# lando terminus -n build:env:delete:pr "$TERMINUS_SITE" --yes
+lando terminus -n multidev:delete $PANTHEON_SITE_NAME.$PANTHEON_ENV --delete-branch
